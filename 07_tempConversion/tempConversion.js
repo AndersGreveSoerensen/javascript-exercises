6da7ec5f6376ab282428ad7c60b1c911661f2ca4
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-03 19:43:43 +0100

    Complete JS exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+function round(numberToRound, amountOfDigits) {
+  let roundedNumber = Math.round(numberToRound * 10**amountOfDigits)/(10**amountOfDigits)
+  return roundedNumber
+}
+
+const convertToCelsius = function(tempInFahrenheit) {
+  let convertedTemp = (tempInFahrenheit - 32) * (5/9)
+  let roundedTemp = round(convertedTemp, 1)
+  return roundedTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  let convertedTemp = tempInCelcius * (9/5) + 32
+  let roundedTemp = round(convertedTemp, 1)
+  return roundedTemp
 };
 
 // Do not edit below this line
