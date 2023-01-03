5070e1577a2e81151887b5a6cd493a903e59c541
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-03 16:56:45 +0100

    Complete JS exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args ) {
 
+    if (args) {
+        for (argument of args) {
+            if (array.includes(argument)) {
+                index = array.indexOf(argument)
+                array.splice(index, 1)
+            }
+        }
+    }
+
+    return array
 };
 
 // Do not edit below this line
