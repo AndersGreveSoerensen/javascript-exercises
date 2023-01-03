bd7ba437f5030e80382c3bece8c0dccc58f6236f
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-03 14:40:21 +0100

    Complete JS 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+function reverseString(string) {
+    let stringReversed = ''
 
+    for (i = 0; i < string.length; i++) {
+        stringReversed += string[string.length - i - 1]
+    }
+    
+    return stringReversed
 };
 
 // Do not edit below this line
