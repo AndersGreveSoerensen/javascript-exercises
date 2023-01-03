f74046e59d11d70f5ab8eaa7911f682a805f1dd7
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-03 13:52:41 +0100

    Complete JS 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringTotal = ""
 
+    for (i = 0; i<num; i++) {
+        stringTotal += string
+    }
+    
+    if (num >= 0) {
+        return stringTotal
+    }
+    else {
+        return "ERROR"
+    }
 };
+
+// repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
+
