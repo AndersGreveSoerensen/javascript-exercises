fba8db313c1bccd3d836adaa57a6e977b49efd4a
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-11 18:33:57 +0100

    complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0
+  for (let arg of array) {
+    sum += arg
+  }
+  return sum
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  finalNumber = 1
+  for (let arg of array) {
+    finalNumber *= arg
+  }
+  return finalNumber
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let finalNumber = 1
+  for (i = 2; i <= a; i++) {
+    finalNumber *= i
+  }
+  return finalNumber
 };
 
 // Do not edit below this line
