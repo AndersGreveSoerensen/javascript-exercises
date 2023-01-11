abff3dc0db4a2251b38f117e72c1a76d21800e3b
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-11 21:56:51 +0100

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (typeof(index) == "string") {
+        index = parseInt(index)
+    }
+    if (index < 0) return "OOPS"
+    else {
+        a = 0
+        b = 1
+        i = 1
+        let fiboNumber = 1
+        while (i < index) {
+            fiboNumber = a + b
+            a = b
+            b = fiboNumber
+            i++
+        }
+        return fiboNumber
+    }
 };
 
 // Do not edit below this line
