e1c0a9b3f203e8eb7132b2592f1c727611c2823b
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-11 19:36:44 +0100

    complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowerCase = string.toLowerCase()
+    let onlyLetters = lowerCase.replace(/[^a-z]/g, "")
+    stringReversed = onlyLetters.split("").reverse().join("")
+    return onlyLetters === stringReversed ? true : false
 };
+
+console.log(palindromes('A car, a man, a maraca.'))
 
 // Do not edit below this line
 module.exports = palindromes;
