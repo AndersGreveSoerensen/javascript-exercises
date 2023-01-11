2fb4d2b72d80ebab8b7d26a889630a9c8f852395
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-11 22:03:55 +0100

    complete exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    bookTitleArray = []
+    array.forEach(element => bookTitleArray.push(element.title));
+    return bookTitleArray
 };
 
 // Do not edit below this line
