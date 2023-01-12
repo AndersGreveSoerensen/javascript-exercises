6fe59987b0fb2f3e6a7fe37776fec8b91cce9d76
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-12 12:00:36 +0100

    complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    array.sort((a, b) => {
+        let lastGuyAge = 0
+        let nextGuyAge = 0
+        
+        if (a.yearOfDeath == undefined) {
+            lastGuyAge = ((new Date()).getFullYear()) - a.yearOfBirth
+        }
+        else {
+            lastGuyAge = a.yearOfDeath - a.yearOfBirth
+        }
 
-};
+        if (b.yearOfDeath == undefined) {
+            nextGuyAge = ((new Date()).getFullYear()) - b.yearOfBirth
+        }
+        else {
+            nextGuyAge = b.yearOfDeath - b.yearOfBirth
+        }
+        return lastGuyAge > nextGuyAge ? -1 : 1
+    }) 
+    return array[0]
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
